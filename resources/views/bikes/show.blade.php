8ac8022d513521de3dc2e22874a123caac7faf25
Jacob Biros <jake@Jacobs-MacBook-Pro.local>
2017-08-12 20:05:52 +0900

    Add return button on card

--- resources/views/bikes/show.blade.php
+++ resources/views/bikes/show.blade.php
@@ -3,44 +3,53 @@
 @section('content')
     <section class="section">
         <div class="container">
-            <div class="show card">
-                <header class="card-header">
-                    <p class="card-header-title">
-                        {{ $bike->code }}
-                    </p>
-                </header>
-                <div class="card-content">
-                    <div class="columns">
-                        <div class="column is-half">
-                            <figure class="image is-4by3">
-                              <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Image">
-                            </figure>
+            <div class="columns">
+                <div class="column is-10 is-offset-1">
+                    <div class="show card">
+                        <header class="card-header">
+                            <p class="card-header-title">
+                                {{ $bike->code }}
+                            </p>
+                            <a href="/bikes" class="card-header-icon">
+                                <span class="icon">
+                                    <i class="fa fa-times"></i>
+                                </span>
+                            </a>
+                        </header>
+                        <div class="card-content">
+                            <div class="columns">
+                                <div class="column is-half">
+                                    <figure class="image is-4by3">
+                                      <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Image">
+                                    </figure>
+                                </div>
+                                <div class="column is-half has-text-centered">
+                                    <p class="title">
+                                        Type: {{ $bike->type }}
+                                    </p>
+                                    <p class="title">
+                                        Color: {{ $bike->color }}
+                                    </p>
+                                    <p class="title">
+                                        Price: ¥500/hr
+                                    </p>
+                                </div>
+                            </div>
                         </div>
-                        <div class="column is-half">
-                            <p class="title">
-                                Type: {{ $bike->type }}
-                            </p>
-                            <p class="title">
-                                Color: {{ $bike->color }}
-                            </p>
-                            <p class="title">
-                                Price: ¥500/hr
-                            </p>
-                        </div>
+                        <footer class="card-footer">
+                            <a class="card-footer-item" href="#">
+                                <span>
+                                    Rent
+                                </span>
+                            </a>
+                            <a class="card-footer-item" href="#">
+                                <span>
+                                    Return
+                                </span>
+                            </a>
+                        </footer>
                     </div>
                 </div>
-                <footer class="card-footer">
-                    <a class="card-footer-item" href="#">
-                        <span>
-                            Rent
-                        </span>
-                    </a>
-                    <a class="card-footer-item" href="#">
-                        <span>
-                            Return
-                        </span>
-                    </a>
-                </footer>
             </div>
         </div>
     </section>
