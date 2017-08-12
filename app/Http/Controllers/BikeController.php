4fdf0e3ab45017cf637db93d6f67b421cae27b11
Jacob Biros <jake@Jacobs-MacBook-Pro.local>
2017-08-12 16:10:53 +0900

    Add route and index function for /bikes page

--- app/Http/Controllers/BikeController.php
+++ app/Http/Controllers/BikeController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Bike;
 use Illuminate\Http\Request;
 
 class BikeController extends Controller
 {
-    //
+    public function index()
+    {
+        $bikes = Bike::all();
+        return view('bikes.index', [ 'bikes' => $bikes ]);
+    }
 }
