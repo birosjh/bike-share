39a771cad15a3c77d1c50961d518481c92a5912b
Jacob Biros <jake@Jacobs-MacBook-Pro.local>
2017-08-12 19:40:02 +0900

    Create page to show individual bike

---
+++ resources/views/bikes/show.blade.php
@@ -0,0 +1,47 @@
+@extends('layout')
+
+@section('content')
+    <section class="section">
+        <div class="container">
+            <div class="show card">
+                <header class="card-header">
+                    <p class="card-header-title">
+                        {{ $bike->code }}
+                    </p>
+                </header>
+                <div class="card-content">
+                    <div class="columns">
+                        <div class="column is-half">
+                            <figure class="image is-4by3">
+                              <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Image">
+                            </figure>
+                        </div>
+                        <div class="column is-half">
+                            <p class="title">
+                                Type: {{ $bike->type }}
+                            </p>
+                            <p class="title">
+                                Color: {{ $bike->color }}
+                            </p>
+                            <p class="title">
+                                Price: ¥500/hr
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <footer class="card-footer">
+                    <a class="card-footer-item" href="#">
+                        <span>
+                            Rent
+                        </span>
+                    </a>
+                    <a class="card-footer-item" href="#">
+                        <span>
+                            Return
+                        </span>
+                    </a>
+                </footer>
+            </div>
+        </div>
+    </section>
+@endsection
