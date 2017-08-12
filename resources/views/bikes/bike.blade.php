3083109d65c20e89bf08912c7299c7e262e85e2a
Jacob Biros <jake@Jacobs-MacBook-Pro.local>
2017-08-12 18:28:17 +0900

    Add a dynamic link that will display a bikes page

--- resources/views/bikes/bike.blade.php
+++ resources/views/bikes/bike.blade.php
@@ -1,18 +1,20 @@
-<div class="card">
-    <header class="card-header">
-    <p class="card-header-title">
-      {{ $bike->code }}
-    </p>
-  </header>
-  <div class="card-image">
-    <figure class="image is-4by3">
-      <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Image">
-    </figure>
-  </div>
-  <div class="card-content">
-    <div class="content">
-        <p>{{ $bike->type }}</p>
-        <p>{{ $bike->color }}</p>
+<a href="/bikes/{{ $bike->id }}">
+    <div class="card">
+        <header class="card-header">
+        <p class="card-header-title">
+          {{ $bike->code }}
+        </p>
+      </header>
+      <div class="card-image">
+        <figure class="image is-4by3">
+          <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Image">
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media-content">
+            <p>{{ $bike->type }}</p>
+            <p>{{ $bike->color }}</p>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
+</a>
