359b953b74d515aee27332c821579e7c574c9d15
Jacob Biros <jake@Jacobs-MacBook-Pro.local>
2017-08-13 16:06:43 +0900

    Add filler columns to keep bike elements in place

--- resources/views/bikes/index.blade.php
+++ resources/views/bikes/index.blade.php
@@ -9,6 +9,11 @@
                         @include('bikes.bike')
                     </div>
                 @endforeach
+                @if($bikes->count() < 4)
+                    @for ($i = 0; $i < 4 - $bikes->count(); $i++)
+                        <div class="column is-one-fifth"></div>
+                    @endfor
+                @endif
             </div>
             {{ $bikes->links() }}
         </div>
