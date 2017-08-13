7a72c83db86901efb9b2150435e3aa8106dee9a8
Jacob Biros <jake@Jacobs-MacBook-Pro.local>
2017-08-13 16:05:31 +0900

    Change pagination count

--- app/Http/Controllers/BikeController.php
+++ app/Http/Controllers/BikeController.php
@@ -11,7 +11,7 @@ class BikeController extends Controller
     // /bikesページを表示する関数
     public function index()
     {
-        $bikes = Bike::where('available', true)->paginate(5);
+        $bikes = Bike::where('available', true)->paginate(4);
         return view('bikes.index', [ 'bikes' => $bikes ]);
     }
 
