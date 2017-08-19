e45e7a6686d3d0f639082fed9442156d246440ac
Jacob Biros <jake@Jacobs-MacBook-Pro.local>
2017-08-19 17:10:56 +0900

    Abstract the logic for checking out and returning a bike

--- app/Bike.php
+++ app/Bike.php
@@ -10,4 +10,24 @@ class Bike extends Model
    {
        return 'code';  // may change into a user name
    }
+
+   public function return()
+   {
+       if($this->available) {
+           return "This bike is not currently checked out";
+       }
+       $this->available = true;
+       //$this->save();
+       return 'Thank you.  Your bike has been returned.';
+   }
+
+   public function checkout()
+   {
+       if(!$this->available) {
+           return "This bike is not currently available";
+       }
+       $this->available = false;
+       //$this->save();
+       return 'Thank you for using our bikes!';
+   }
 }
