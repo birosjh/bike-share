d2f8161b7dcc2f403f19a00610c2cfbfdc76637e
Jacob Biros <jake@Jacobs-MacBook-Pro.local>
2017-08-19 15:12:49 +0900

    Move non-resourceful functions to PagesController

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function return()
+    {
+        return view('return');
+    }
+
+    public function unavailable()
+    {
+        return view('unavailable');
+    }
+}
