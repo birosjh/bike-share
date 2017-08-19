b17c87a94c2a491ac0ad709966eaa72856e46b27
Jacob Biros <jake@Jacobs-MacBook-Pro.local>
2017-08-19 17:10:15 +0900

    Create a root id for the vue model, include app.js and make the csrf token available for ajax requests

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Laravel</title>
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.5.1/css/bulma.css">
@@ -13,8 +13,9 @@
     </head>
     <body>
         @include('navbar')
-        <div class="flex-center position-ref full-height">
+        <div id="app">
             @yield('content')
         </div>
+        <script src="{{ asset('js/app.js') }}" charset="utf-8"></script>
     </body>
 </html>
