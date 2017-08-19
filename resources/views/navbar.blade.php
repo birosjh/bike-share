612eabc9851d5a75343c38b7524b822d4c1cda7b
Jacob Biros <jake@Jacobs-MacBook-Pro.local>
2017-08-19 14:08:46 +0900

    Add login and registration buttons to the navigation bar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -4,4 +4,28 @@
             <h1 class="title">Jitenshare</h1>
         </div>
     </div>
+    <div class="navbar-end">
+        <div class="navbar-item">
+            @if (Auth::guest())
+                <li><a href="{{ route('login') }}">Login</a></li>
+                <li><a href="{{ route('register') }}">Register</a></li>
+            @else
+                <div class="navbar-item has-dropdown is-hoverable">
+                    <a href="#" class="navbar-link">
+                        {{ Auth::user()->name }} <span class="caret"></span>
+                    </a>
+                    <div class="navbar-dropdown">
+                            <a class="navbar-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                                Logout
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                    </div>
+                </div>
+            @endif
+        </div>
+    </div>
 </nav>
